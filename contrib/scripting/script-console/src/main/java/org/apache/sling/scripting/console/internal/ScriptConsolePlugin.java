2853d5184c39a4ca4244ff9790dd6d67415e2150
Chetan Mehrotra <chetanm@adobe.com>
2012-05-03 00:16:29 +0530

    Adding support for handling code passed as part of multi part content

--- contrib/scripting/script-console/src/main/java/org/apache/sling/scripting/console/internal/ScriptConsolePlugin.java
+++ contrib/scripting/script-console/src/main/java/org/apache/sling/scripting/console/internal/ScriptConsolePlugin.java
@@ -17,27 +17,31 @@
 
 package org.apache.sling.scripting.console.internal;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.io.IOUtils;
 import org.apache.felix.scr.annotations.*;
+import org.apache.felix.webconsole.AbstractWebConsolePlugin;
 import org.apache.felix.webconsole.DefaultVariableResolver;
 import org.apache.felix.webconsole.SimpleWebConsolePlugin;
 import org.apache.felix.webconsole.WebConsoleUtil;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.scripting.ScriptEvaluationException;
 import org.apache.sling.api.scripting.SlingBindings;
 import org.apache.sling.api.scripting.SlingScript;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.io.JSONWriter;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * User: chetanm
@@ -48,6 +52,7 @@ import org.osgi.framework.BundleContext;
 @Service
 @Property(name = "felix.webconsole.label", value = ScriptConsolePlugin.NAME)
 public class ScriptConsolePlugin extends SimpleWebConsolePlugin {
+    private Logger log = LoggerFactory.getLogger(getClass());
     public static final String NAME = "scriptconsole";
     private static final String TITLE = "%script.title";
     private static final String[] CSS = {"/res/ui/codemirror/lib/codemirror.css","/res/ui/script-console.css"};
@@ -79,12 +84,12 @@ public class ScriptConsolePlugin extends SimpleWebConsolePlugin {
         if (contentType.startsWith("text/")) {
             resp.setCharacterEncoding("UTF-8");
         }
-
+        final String script = getCodeValue(req);
         final SlingBindings bindings = new SlingBindings();
         final PrintWriter pw = resp.getWriter();
         //Populate bindings
         bindings.put(SlingBindings.REQUEST, req);
-        bindings.put(SlingBindings.READER, req.getReader());
+        bindings.put(SlingBindings.READER, new StringReader(script));
         bindings.put(SlingBindings.RESPONSE, resp);
         bindings.put(SlingBindings.OUT, pw);
 
@@ -92,16 +97,34 @@ public class ScriptConsolePlugin extends SimpleWebConsolePlugin {
         //enclosing OSGi container
         bindings.put("bundleContext", bundleContext);
 
-        final String script = WebConsoleUtil.getParameter(req, "code");
+
+
         final String lang = WebConsoleUtil.getParameter(req, "lang");
         final Resource resource = new RuntimeScriptResource(lang, script);
+        final boolean webClient = "webconsole".equals(WebConsoleUtil.getParameter(req,"client"));
 
         SlingScript slingScript = resource.adaptTo(SlingScript.class);
         try {
+            log.debug("Executing script {}",script);
             slingScript.eval(bindings);
         } catch (Throwable t){
+            if(!webClient){
+                resp.setStatus(500);
+            }
             pw.println(exceptionToString(t));
+            log.warn("Error in executing script",t);
         }
+    }
+
+    private String getCodeValue(HttpServletRequest req) throws IOException {
+        String script = WebConsoleUtil.getParameter(req, "code");
+        if(script == null){
+            script = getContentFromFilePart(req, "code");
+        }
+        if(script == null){
+            throw new IllegalArgumentException("'code' parameter not passed");
+        }
+        return script;
     }
 
     private String getContentType(HttpServletRequest req) {
@@ -169,6 +192,43 @@ public class ScriptConsolePlugin extends SimpleWebConsolePlugin {
             return "javascript";
         }
         return null;
+    }
+
+    private String getContentFromFilePart(HttpServletRequest req, String paramName) throws IOException {
+        String value = WebConsoleUtil.getParameter(req, paramName);
+        if(value != null){
+            return value;
+        }
+        final Map params = (Map) req.getAttribute( AbstractWebConsolePlugin.ATTR_FILEUPLOAD );
+        if ( params == null ){
+            return null;
+        }
+        FileItem[] codeFile = getFileItems(params,paramName);
+        if(codeFile.length == 0){
+            return null;
+        }
+        InputStream is = null;
+        try{
+            is = codeFile[0].getInputStream();
+            StringWriter sw = new StringWriter();
+            IOUtils.copy(is,sw,"utf-8");
+            return sw.toString();
+        }finally{
+            IOUtils.closeQuietly(is);
+        }
+    }
+
+    private FileItem[] getFileItems(Map params, String name) {
+        final List files = new ArrayList();
+        FileItem[] items = (FileItem[]) params.get(name);
+        if (items != null) {
+            for (int i = 0; i < items.length; i++) {
+                if (!items[i].isFormField() && items[i].getSize() > 0) {
+                    files.add(items[i]);
+                }
+            }
+        }
+        return (FileItem[]) files.toArray(new FileItem[files.size()]);
     }
 
     @Activate
