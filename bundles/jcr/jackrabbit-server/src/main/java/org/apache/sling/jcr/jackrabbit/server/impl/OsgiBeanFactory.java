c2f79d75386a0e7d119c6d8aa0e746ef2dc1ae00
Chetan Mehrotra <chetan.mehrotra@gmail.com>
2012-09-06 21:58:44 +0530

    Adding support for dynamically determining the dependencies and then use Felix Dependency manager to manage them

--- bundles/jcr/jackrabbit-server/src/main/java/org/apache/sling/jcr/jackrabbit/server/impl/OsgiBeanFactory.java
+++ bundles/jcr/jackrabbit-server/src/main/java/org/apache/sling/jcr/jackrabbit/server/impl/OsgiBeanFactory.java
@@ -16,23 +16,31 @@
  */
 package org.apache.sling.jcr.jackrabbit.server.impl;
 
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.Service;
-import org.apache.jackrabbit.core.config.BeanConfig;
-import org.apache.jackrabbit.core.config.BeanFactory;
-import org.apache.jackrabbit.core.config.ConfigurationException;
-import org.apache.jackrabbit.core.config.SimpleBeanFactory;
-import org.apache.jackrabbit.core.security.user.action.AuthorizableAction;
+import org.apache.felix.dm.Component;
+import org.apache.felix.dm.Dependency;
+import org.apache.felix.dm.DependencyManager;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.jackrabbit.core.config.*;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.InputSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.apache.jackrabbit.core.config.RepositoryConfigurationParser.REPOSITORY_HOME_VARIABLE;
 
 /**
  * An OSGi aware BeanFactory implementation. It pulls in dependencies from OSGi and provides it
  * to jackrabbit
  */
-@Component
-@Service
+@org.apache.felix.scr.annotations.Component
 public class OsgiBeanFactory implements BeanFactory{
     private static final String FACTORY_TYPE_OSGI = "osgi";
 
@@ -40,18 +48,103 @@ public class OsgiBeanFactory implements BeanFactory{
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
-    @Reference
-    private AuthorizableAction authorizableAction;
+    private Set<String> classNames = new HashSet<String>();
+    private Map<String,Class> classNameMapping = new HashMap<String,Class>();
+    private Set<Class> dependencies = new HashSet<Class>();
+    private Map<Class,Object> instances = new ConcurrentHashMap<Class, Object>();
+
+    @Activate
+    private void activate(BundleContext context) throws IOException, ConfigurationException {
+        parseAndReadConfig(context);
+
+        log.info("Would be finding implementations for "+ dependencies);
+        DependencyManager dm = new DependencyManager(context);
+        Component c = dm.createComponent();
+
+        c.setInterface(BeanFactory.class.getName(),new Properties())
+         .setImplementation(this);
+
+        for(Class clazz : dependencies){
+            Dependency d = dm.createServiceDependency()
+              .setService(clazz)
+              .setRequired(true)
+              .setCallbacks(this,"added","removed");
+
+            c.add(d);
+        }
+
+        dm.add(c);
+    }
+
+    /**
+     * A hacky incorrect way to read the config. For proper implementation we should read config as managed in
+     * org.apache.sling.jcr.jackrabbit.server.impl.SlingServerRepository#acquireRepository()
+     */
+    private void parseAndReadConfig(BundleContext context) throws ConfigurationException, IOException {
+        URL u = context.getBundle().getResource("repository.xml");
+        Properties variables = new Properties(System.getProperties());
+        variables.setProperty(REPOSITORY_HOME_VARIABLE, "");
+        RepositoryConfigurationParser parser = new RepositoryConfigurationParser(variables);
+        parser.setConfigVisitor(new DepFinderBeanConfigVisitor());
+
+        InputStream is = null;
+
+        try{
+            is = u.openStream();
+            parser.parseRepositoryConfig(new InputSource(is));
+        }finally{
+            if(is != null){
+                is.close();
+            }
+        }
+    }
 
     public Object newInstance(Class<?> klass, BeanConfig config) throws ConfigurationException {
         if(FACTORY_TYPE_OSGI.equals(config.getFactoryType())){
-            if(klass.isAssignableFrom(AuthorizableAction.class)){
-                return authorizableAction;
+            if(instances.containsKey(klass)){
+                return instances.get(klass);
             }
 
             throw new ConfigurationException("No reference configured for type "+klass.getName());
         }
 
         return delegate.newInstance(klass,config);
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    private void added(ServiceReference serviceReference,Object o){
+        //Use OBJECTCLASS property from SR as that determines under what classes
+        //a given service instance is published
+        //Class[] interfaces = o.getClass().getInterfaces();
+        String[] interfaces = (String[]) serviceReference.getProperty(Constants.OBJECTCLASS);
+        for(String intf : interfaces){
+            if(classNames.contains(intf)){
+                instances.put(classNameMapping.get(intf),o);
+            }
+        }
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    private void removed(Object o){
+        instances.remove(o);
+    }
+
+
+
+    private class DepFinderBeanConfigVisitor implements BeanConfigVisitor {
+
+        public void visit(BeanConfig config) {
+            if(FACTORY_TYPE_OSGI.equals(config.getFactoryType())){
+                String className = config.getClassName();
+                classNames.add(className);
+                try {
+                    Class clazz = getClass().getClassLoader().loadClass(className);
+                    dependencies.add(clazz);
+                    classNameMapping.put(className,clazz);
+                } catch (ClassNotFoundException e) {
+                    log.warn("Could not load class for "+className,e);
+                }
+            }
+        }
     }
 }
